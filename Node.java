9e8249d7eaec7e6a726768c047f9bccc593f13ab
gheefop <zitbam@gmail.com>
2017-11-15 18:03:00 -0800

    Node getters and setters

--- Node.java
+++ Node.java
@@ -1,48 +1,80 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package dictionaryex;
-
-/**
+/** An object containing a key, value, and boolean flag.
  * @author despa
- * @param <K>
- * @param <V>
+ * @author Daniel J. Norment
+ * @version 1.1
  */
 public class Node<K extends Comparable<? super K>,V> implements Comparable<Node<K, V>>
 {
-	K key;
-	V value;
-	int flag;
-	
-	public Node() {
-		key = null;
-		value = null;
-		flag = 0;
-	}
+    private K key;
+    private V value;
+    private boolean flag;
+    
+    public Node() 
+    {
+        key = null;
+        value = null;
+        flag = false;
+    }
 
-	public Node(K ke, V val)
-	{
-		this.key = ke;
-		this.value = val;
-		this.flag = 1;
-	
-	}
+    public Node(K key, V value)
+    {
+        this.key = key;
+        this.value = value;
+        this.flag = false;
+    }
+    
+    /** Set's this node's key.
+     *  @param key  The key to which to change this node's key. */
+    public void setKey(K key)
+    {
+        this.key = key;
+    }
+    
+    /** Returns this node's key.
+     *  @return This node's key. */
+    public K getKey()
+    {
+        return key;
+    }
+    
+    /** Set's this node's value.
+     *  @param value  The value to which to change this node's value. */
+    public void setValue(V value)
+    {
+        this.value = value;
+    }
+    
+    /** Returns this node's value.
+     *  @return This node's value. */
+    public V getValue()
+    {
+        return value;
+    }
+    
+    /** Sets the node's flag to indicate data has been removed. */
+    public void setRemoved()
+    {
+        flag = true;
+    }
         
-        public void setFlagR()
-        {
-            this.flag = 0;
-        }
-        
-        public void setFlagA()
-        {
-            this.flag = 1;
-        }
+    /** Sets the node's flag to indicate it has not been used. */
+    public void setUnused()
+    {
+        flag = false;
+    }
+    
+    /** Returns this node's flag.
+     *  @return This node's flag. */
+    public boolean getFlag()
+    {
+        return flag;
+    }
 
-        @Override
-        public int compareTo(Node<K,V> nod)
-        {
-            return key.compareTo(nod.key);
-        }
+    /** Compares keys between two nodes.
+     *  @param otherNode  The node to which to compare this node.
+     *  @return -1, 0, or 1 if the key in this node is less than, equal to, or greater than the key of otherNode. */
+    public int compareTo(Node<K,V> otherNode)
+    {
+        return key.compareTo(otherNode.getKey());
+    }
 }
